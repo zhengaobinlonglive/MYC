f5efc1511222df8bdd0effc6fef1cfd95bbc9c31
zhengaobinlonglive <56379790+zhengaobinlonglive@users.noreply.github.com>
2023-11-13 20:17:14 +0800

    Add files via upload

---
+++ miniprogram/pages/haha/haha.ts
@@ -0,0 +1,70 @@
+// pages/haha/haha.ts
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    info : '我是哈哈',
+    infos : '我是嘻嘻',
+    imgSrc : 'https://img1.baidu.com/it/u=716638254,3920932970&fm=253&fmt=auto&app=138&f=JPEG?w=889&h=500',
+    haha : Math.random() * 20, // 生成20以内的数据
+    haha1 : Math.random().toFixed(2)
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
